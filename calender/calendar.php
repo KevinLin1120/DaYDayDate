83772284733dcfc0ec267c785d393721581bb4bd
howardchou <howard9107@gmail.com>
2022-05-28 20:56:07 +0800

    0528月曆套進版面 但會發生跑版情況

--- calender/calendar.php
+++ calender/calendar.php
@@ -11,8 +11,55 @@
 
     <link rel="stylesheet" href="style.css">
 
-    <script>
+    <style>
+      label.logo{
+        color:black;
+        font-size: 36px;
+        line-height: 80px;
+        padding: 0 50px;
+        font-weight: bold;
+      }
+      nav{
+          background-color: white;
+          height: 80px;
+          margin: 0;
+          padding: 0;
+          border-bottom: 1px solid black;
+      }
+      .side-menu{
+          width: 20%;
+          height:600px;
+          background-color:#394B6F;
+      }
+      .calender-back{
+          background-color: lightgray;
+          height: 600px;
+          width:60%; 
+          border-radius:20px;
+          overflow: flex;
+          margin: auto;
+      } 
+      .flex-container {
+      display: flex;
+      flex-direction: row;
+      }
+      input[type=search] {
+      border: 1px solid #000;
+      width: 200px;
+      padding: 4px 12px;
+      border-radius: 20px;
+      -webkit-appearance: none;
+      margin-left: 40px;
+      margin-top: 10px;
+      } 
+      i{
+          float: right;
+          margin-right: 20px;
+          margin-top: 5px
+      }
+    </style>
 
+    <script>
       document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -34,24 +81,31 @@
         });
         calendar.render();
       });
-
     </script>
-
   </head>
 
   <body>
     
   
     <div class="container">
+      <header>
+          <nav>
+              <label class="logo">DayDay Date</label>
+              <i class="fa-solid fa fa-bell-o fa-2xl" aria-hidden="true"></i>
+          </nav>
+      </header>
 
-      <div id='addEvent'>
-        <?php
-            include("./card/cards.html");
-          ?>
-      </div>
-      
+      <div class="flex-container">
+        <div class="side-menu">
+          <form>
+              <input type="search"placeholder="Search friends...">
+          </form>
+          <aside>
+          </aside>
+        </div>
+        <div class="calender-back">
         <div id='calendar'></div>
-
+      </div>
     </div>
   </body>
 
