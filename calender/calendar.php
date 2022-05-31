40fca16025a1ad6632fd9053bd226721885a0453
howardchou <howard9107@gmail.com>
2022-05-31 22:12:39 +0800

    0531按鈕已放入
    帶改CARD格式

--- calender/calendar.php
+++ calender/calendar.php
@@ -11,7 +11,6 @@
     
     <script src="./src/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet"  href="./style.css">
-    <link rel="stylesheet"  href="./card/cards.css">
 
     <style>
       label.logo{
@@ -72,7 +71,7 @@
           width: 80px;
           height: 30px;
           border-radius: 50px;
-
+          background-color: lightblue;
           /* font-family: 'Inter'; */
           /* font-style: normal; */
           text-align: center;
@@ -92,6 +91,58 @@
           padding: 0 30px;
           border-top: none;
       }
+
+      /*card */
+      /*
+      .setCard{
+            position: absolute;
+            width: 390px;
+            height: 600px;
+            left: 200px;
+            top: 30px;
+            padding: 20px;
+            background: #FFFFFF;
+            border-radius: 70px;*/
+            /* border: 1px solid black; */
+        /*}
+        #isPublic{
+            width: 30px;
+            height: 30px;
+            background-color: transparent;
+            border: none;
+            background-image: url('./images/unlock.svg');
+        }
+        .modal-header .close{
+            margin: 1px;
+            padding: 1px;
+        }
+        .close > span{
+            font-size: 32px;
+        }
+        .test{
+            width: 100%;
+            height: 131px;*/
+            /* left: 676px;
+            top: 535px; */
+
+            /*padding: 20px;
+            background: #E5E5E5;
+            border-radius: 30px;
+            border: none;
+
+            overflow: auto;
+            resize: vertical;
+        }
+        #cardInput > tbody > tr > td > input{
+            border: none;
+            border-bottom: 2px solid black;
+        }
+        #cardInput > tbody > tr > td{
+            padding-bottom: 14px;
+            
+            font-weight: 600;
+            font-size: 16px;
+        }*/
       
     </style>
 
@@ -148,6 +199,57 @@
         <div class="calender-back">
         <div id='calendar'></div>
       </div>
+
+
+      <!-- card -->
+      <div class="modal fade" id="editCard" tabindex="-1" aria-labelledby="cardModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content setCard">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <!-- <h5 class="modal-title" id="cardModalLabel">Modal title</h5> -->
+                    <button id="isPublic"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="" method="post" name="eventCard">
+                        <table id="cardInput">
+                            <tr>
+                                <td>標題：</td>
+                                <td><input type="text" name="title"></td>
+                            </tr>
+                            <tr>
+                                <td>起始日期：</td>
+                                <td><input type="datetime-local" name="startDT"></td>
+                            </tr>
+                            <tr>
+                                <td>結束日期：</td>
+                                <td><input type="datetime-local" name="endDT"></td>
+                            </tr>
+                            <tr>
+                                <td>連結：</td>
+                                <td><input type="text" name="link"></td>
+                            </tr>
+                            <tr>
+                                <td>邀請：</td>
+                                <td><input type="text" name="invite"></td>
+                            </tr>
+                            <tr>
+                                <td>顏色分類：</td>
+                                <td><input type="color" name="color"></td>
+                            </tr>
+                        </table>
+                        <textarea class="test" rows="3" placeholder="備註： "></textarea>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-cancel" data-dismiss="modal">取消</button>
+                  <button type="button" class="btn btn-confirm">儲存</button>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
   </body>
 
