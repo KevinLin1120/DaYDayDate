4fc695bf49c466c756a7133d50238198c55cd99c
howardchou <howard9107@gmail.com>
2022-06-02 00:50:00 +0800

    0601update
    Card's background still a problem
    other is all fine
    search bar is next to do

--- calender/calendar.php
+++ calender/calendar.php
@@ -69,15 +69,15 @@
       /* buttons */
       .btn{
           width: 80px;
-          height: 30px;
-          border-radius: 50px;
-          background-color: lightblue;
+          height: 33px;
+          border-radius: 30px;
+          background-color: darkgray;
           /* font-family: 'Inter'; */
           /* font-style: normal; */
           text-align: center;
           font-weight: 600;
           font-size: 15px;
-          color: blue;
+          color: black;
       }
       .btn-cancel{
           background: red;
@@ -93,24 +93,26 @@
       }
 
       /*card */
-      /*
+      .CARD{
+        display: none;
+      }
       .setCard{
             position: absolute;
             width: 390px;
             height: 600px;
-            left: 200px;
-            top: 30px;
+            left: 500px;
+            top: 100px;
             padding: 20px;
-            background: #FFFFFF;
-            border-radius: 70px;*/
-            /* border: 1px solid black; */
-        /*}
+            background: white;
+            border-radius: 70px;
+            border: 1px solid black;
+        }
         #isPublic{
             width: 30px;
             height: 30px;
             background-color: transparent;
             border: none;
-            background-image: url('./images/unlock.svg');
+            background-image: url('./card/images/unlock.svg');
         }
         .modal-header .close{
             margin: 1px;
@@ -121,12 +123,12 @@
         }
         .test{
             width: 100%;
-            height: 131px;*/
-            /* left: 676px;
-            top: 535px; */
+            height: 131px;
+            left: 676px;
+            top: 535px;
 
-            /*padding: 20px;
-            background: #E5E5E5;
+            padding: 20px;
+            background: wheat;
             border-radius: 30px;
             border: none;
 
@@ -142,7 +144,7 @@
             
             font-weight: 600;
             font-size: 16px;
-        }*/
+        }
       
     </style>
 
@@ -162,16 +164,25 @@
           events:[
             {title:"test1",start:"2022-0501",end:""},
             {title:"test2",start:"2022-0502T11:00:00",end:"2022-0502T12:00:00"},
-            {title:"test3",start:"2022-0509T11:00:00",end:"2022-0511T11:00:00"}
+            {title:"test3",start:"2022-0509T11:00:00",end:"2022-0511T11:00:00"},
+            {title:"test4",start:"2022-0510T11:00:00",end:"2022-0511T11:00:00"}
           ]
 
         });
         calendar.render();
         
       });
-      $(document).ready(
-        function(){
-          $("#fc-dom-1").append('<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editCard">新增行程</button>');
+      $(document).ready(function(){
+          $("#fc-dom-1").append('<button type="button" class="btn btn-primary" id="add" data-toggle="modal" data-target="#editCard">新增行程</button>');
+          $('#add').click(function(){
+            $('.CARD').show();
+          });
+          $('.close').click(function(){
+            $('.CARD').hide();
+          });
+          $('#btn btn-cancel').click(function(){
+            $('.CARD').hide();
+          });
         }
       );
     </script>
@@ -197,59 +208,62 @@
           </aside>
         </div>
         <div class="calender-back">
-        <div id='calendar'></div>
+          <div id='calendar'></div>
+        </div>
       </div>
 
 
       <!-- card -->
-      <div class="modal fade" id="editCard" tabindex="-1" aria-labelledby="cardModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-            <div class="modal-content setCard">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <!-- <h5 class="modal-title" id="cardModalLabel">Modal title</h5> -->
-                    <button id="isPublic"></button>
-                </div>
-                <div class="modal-body">
-                    <form action="" method="post" name="eventCard">
-                        <table id="cardInput">
-                            <tr>
-                                <td>標題：</td>
-                                <td><input type="text" name="title"></td>
-                            </tr>
-                            <tr>
-                                <td>起始日期：</td>
-                                <td><input type="datetime-local" name="startDT"></td>
-                            </tr>
-                            <tr>
-                                <td>結束日期：</td>
-                                <td><input type="datetime-local" name="endDT"></td>
-                            </tr>
-                            <tr>
-                                <td>連結：</td>
-                                <td><input type="text" name="link"></td>
-                            </tr>
-                            <tr>
-                                <td>邀請：</td>
-                                <td><input type="text" name="invite"></td>
-                            </tr>
-                            <tr>
-                                <td>顏色分類：</td>
-                                <td><input type="color" name="color"></td>
-                            </tr>
-                        </table>
-                        <textarea class="test" rows="3" placeholder="備註： "></textarea>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-cancel" data-dismiss="modal">取消</button>
-                  <button type="button" class="btn btn-confirm">儲存</button>
-                </div>
-            </div>
+      <div class ="CARD">
+        <div class="modal fade" id="editCard" tabindex="-1" aria-labelledby="cardModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+              <div class="modal-content setCard">
+                  <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                      </button>
+                      <!-- <h5 class="modal-title" id="cardModalLabel">Modal title</h5> -->
+                      <button id="isPublic"></button>
+                  </div>
+                  <div class="modal-body">
+                      <form action="" method="post" name="eventCard">
+                          <table id="cardInput">
+                              <tr>
+                                  <td>標題：</td>
+                                  <td><input type="text" name="title"></td>
+                              </tr>
+                              <tr>
+                                  <td>起始日期：</td>
+                                  <td><input type="datetime-local" name="startDT"></td>
+                              </tr>
+                              <tr>
+                                  <td>結束日期：</td>
+                                  <td><input type="datetime-local" name="endDT"></td>
+                              </tr>
+                              <tr>
+                                  <td>連結：</td>
+                                  <td><input type="text" name="link"></td>
+                              </tr>
+                              <tr>
+                                  <td>邀請：</td>
+                                  <td><input type="text" name="invite"></td>
+                              </tr>
+                              <tr>
+                                  <td>顏色分類：</td>
+                                  <td><input type="color" name="color"></td>
+                              </tr>
+                          </table>
+                          <textarea class="test" rows="3" placeholder="備註： "></textarea>
+                      </form>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-cancel" data-dismiss="modal">取消</button>
+                    <button type="button" class="btn btn-confirm">儲存</button>
+                  </div>
+              </div>
+          </div>
         </div>
-    </div>
+      </div>
     </div>
   </body>
 
