feee0c9c4c2ae5cd185434525920e55453acf1f7
howardchou <howard9107@gmail.com>
2022-06-05 22:38:11 +0800

    0605 calander update

--- calender/calendar.php
+++ calender/calendar.php
@@ -3,16 +3,21 @@
   <head>
     <meta charset='utf-8' />
     <link href='./src/main.css' rel='stylesheet'>
+    <link rel="stylesheet"  href="./style.css">
+    <link rel="stylesheet"  href="./card/cards.css">
+
     <script src='src/main.js'></script>
 
     <script src="src/locales-all.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- <script src="card/cards.html"></script> -->
-    
+    <!-- <script src="card/cards.html"></script> --><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
     <script src="./src/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet"  href="./style.css">
+
 
     <style>
+      
       label.logo{
         color:black;
         font-size: 36px;
@@ -35,7 +40,6 @@
       .calender-back{
           background-color: lightgray;
           height: 650px;
-          width:70%; 
           border-radius:20px;
           overflow: flex;
           margin: auto;
@@ -50,8 +54,8 @@
       padding: 4px 12px;
       border-radius: 20px;
       -webkit-appearance: none;
-      margin-left: 40px;
-      margin-top: 10px;
+      /* margin-left: 40px; */
+      /* margin-top: 10px; */
       } 
       i{
           float: right;
@@ -68,7 +72,7 @@
       
       /* buttons */
       .btn{
-          width: 80px;
+          width: 90px;
           height: 33px;
           border-radius: 30px;
           background-color: darkgray;
@@ -100,7 +104,7 @@
             position: absolute;
             width: 390px;
             height: 600px;
-            left: 500px;
+            /*left: 500px;*/
             top: 100px;
             padding: 20px;
             background: white;
@@ -177,21 +181,29 @@
           $('#add').click(function(){
             $('.CARD').show();
           });
-          $('.close').click(function(){
-            $('.CARD').hide();
-          });
-          $('#btn btn-cancel').click(function(){
-            $('.CARD').hide();
-          });
         }
       );
+
+      $(document).ready(function(){
+            var isPublic = true;
+            $("#isPublic").mouseup(function(){
+                if(isPublic){ // If it's public, switch to private
+                    $(this).css("background-image", 'url(./card/images/lock.svg)');
+                    isPublic = false;
+                }
+                else{ // If it's private, switch to public
+                    $(this).css("background-image", 'url(./card/images/unlock.svg)');
+                    isPublic = true;
+                }
+            });
+        });
     </script>
   </head>
 
   <body>
     
   
-    <div class="container">
+    <div class="Container">
       <header>
           <nav>
               <label class="logo">DayDay Date</label>
